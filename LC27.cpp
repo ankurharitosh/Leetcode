15dfcd13dc812897818ea5e845630a0dd43f791a
ankur248 <ankurharitosh@gmail.com>
2019-09-25 19:28:17 +0530

    Add existing file

---
+++ LC27.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n= nums.size(), i=0;
+        
+        while(i<n){
+            if(nums[i] == val){
+                swap(nums[i], nums[n-1]);
+                n--;
+            }
+            else
+                i++;
+        }
+        return n;
+    }
+};
